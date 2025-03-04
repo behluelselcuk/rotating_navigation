697f841437946ac09b294053d4c1a16fd19dc64e
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-03-04 17:48:28 +0100

    Erstellen des JS-Codes für die Rotationdsanimation
    Die Funktionen und Event-Listeners für die
    Erstellung der Animation des Aus-und Einklappens
    des Menüs wurden erfstellt

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ let closeBtn = document.querySelector('#close');
 
 
 // VEARBEITUNG
+function rollOut() {
+    if (
+        !contentPage.classList.contains('show-nav') &&
+        !menuCircle.classList.contains('show-nav') &&
+        !navBar.classList.contains('show-nav')
+    ) {
+        contentPage.classList.add('show-nav');
+        menuCircle.classList.add('show-nav');
+        navBar.classList.add('show-nav');
+    }
+}
+
+function rollUp() {
+    if (
+        contentPage.classList.contains('show-nav') &&
+        menuCircle.classList.contains('show-nav') &&
+        navBar.classList.contains('show-nav')
+    ) {
+        contentPage.classList.remove('show-nav');
+        menuCircle.classList.remove('show-nav');
+        navBar.classList.remove('show-nav');
+    }
+}
+
+menuBtn.addEventListener('click', rollOut);
+
+closeBtn.addEventListener('click', rollUp);
