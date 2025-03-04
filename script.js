8b328216b6f7e6784f49474df3e5cf1d82f2c5db
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-03-04 17:37:17 +0100

    Einbauen des Ziels und der Eingaben
    Das Ziel in JS wurde definiert und die Eingabe-
    Elemente deklariert und initialisiert

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict'
 
+// ZIEL
+// Beim Klicken auf den Burger-Menü-Button
+//     'main', 'circle' & 'nav' sollen die Klasse '.show-nav' dazubekommen
+// Beim Klicken auf den X-Button
+//     die '.show-nav'-Klasse soll entfernt werden
+
+
+
+// Eingabe
+let contentPage = document.querySelector('main');
+let menuCircle = document.querySelector('.circle');
+let navBar = document.querySelector('nav');
+let menuBtn = document.querySelector('#open');
+let closeBtn = document.querySelector('#close');
+
+
+// VEARBEITUNG
